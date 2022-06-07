945aa75f07f0c12b00734b769ca93731461b081b
PalomaSelva <palomaselva1@gmail.com>
2022-06-06 22:49:27 -0300

    final

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,25 @@
 var number=document.getElementById('number1');
 var n1=prompt("Digite um número:");
+n1=parseInt(n1)
 number.innerHTML= n1
 var number2=document.getElementById('number2');
 var n2=prompt("Digite um número:");
+n2=parseInt(n2)
 number2.innerHTML= n2
-
-
 
 function sintaxe(a,b) {
     sintese=document.getElementById('sintese')
     if (a===b) {
-        document.write("Os números ",a," e ",b," são iguais. ")
+        x=(`Os números ${a} e ${b} são iguais. `)
+        sintese.innerHTML=x
     } else {
-       document.write("Os números ",a," e ",b," não são iguais. ") 
+       x=(`Os números ${a} e ${b} não são iguais. `) 
+       sintese.innerHTML=x
     }
 
 }
 function soma(a,b) {
+    sintese=document.getElementById('sintese')
     soma=a+b
     console.log(soma)
     if (soma>10) {
@@ -30,13 +33,10 @@ function soma(a,b) {
     } else {
         soma20="menor"
     }
-    document.write("Sua soma é ",soma,", que é ",soma10," que 10 e ",soma20," que 20.")
+        Y=("Sua soma é "+soma+", que é "+soma10+" que 10 e "+soma20+" que 20.")
+        sintese.innerHTML=Y
 }
 
-
-
-
-
-sintaxe(n,n2)
-soma(n,n2)
+sintaxe(n1,n2)
+soma(n1,n2)
 
